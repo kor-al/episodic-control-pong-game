cccca0880d9252fe8f2f17a06bbe6ff6ad68ef1d
kor-al <korinalice@gmail.com>
2017-02-22 23:31:39 +0300

    Update HistoryRecorder.h

--- Source/PongGame/HistoryRecorder.h
+++ Source/PongGame/HistoryRecorder.h
@@ -1,11 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
-#include "D:/Alice/Documents/Unreal Projects/PongGame/ThirdParty/OpenCV/Includes/opencv2/core/core.hpp"
-#include "D:/Alice/Documents/Unreal Projects/PongGame/ThirdParty/OpenCV/Includes/opencv2/core/mat.hpp"
-#include "D:/Alice/Documents/Unreal Projects/PongGame/ThirdParty/OpenCV/Includes/opencv2/highgui.hpp"	
-#include "D:/Alice/Documents/Unreal Projects/PongGame/ThirdParty/OpenCV/Includes/opencv2/imgproc.hpp"
 #include "Score.h"
 #include <vector>
 
